dc43a79bebdac23821c4ccd6f12a1d45288f83a7
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-10-09 08:37:17 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const serveIndex = require('serve-index');
+const express = require('express');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => cb(null, file.originalname),
+  destination: (req, file, cb) => cb(null, './public/files'),
+})
+const upload = multer({storage});
+const app = express();
+app.post('/upload', upload.single('file'), (req, res) => {
+	res.redirect(`${req.protocol}://${req.get('host')}/files`);
+});
+app.use(express.static(__dirname + '/public'));
+app.use(serveIndex('public', {'icons': true}));
+app.listen(3000);
